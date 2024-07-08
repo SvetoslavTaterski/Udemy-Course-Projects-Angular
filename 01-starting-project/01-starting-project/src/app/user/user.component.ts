788726265b82c3fa954415e44d2acd6577065e28
SvetoslavTaterski <svetoslav0247@gmail.com>
2024-07-08 17:33:51 +0300

    Using @Input decorator

--- 01-starting-project/01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/01-starting-project/src/app/user/user.component.ts
@@ -1,29 +1,19 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-    selectedUser = signal(DUMMY_USERS[randomIndex]);
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-    //With signal
-    imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
-    //Without signal
-    // get imagePath() {
-    //   return 'assets/users/' + this.selectedUser.avatar;
-    // }
-
-    onSelectUser() {
-      const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-      this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    }
+  onSelectUser() {}
 }
