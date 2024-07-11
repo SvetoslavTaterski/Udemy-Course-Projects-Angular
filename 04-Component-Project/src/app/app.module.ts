bb0c1eec222607b5b08a2a2d65c55d1cbf21913e
SvetoslavTaterski <svetoslav0247@gmail.com>
2024-07-11 15:59:41 +0300

    Creating a reusable component

--- 04-Component-Project/src/app/app.module.ts
+++ 04-Component-Project/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from "@angular/platform-browser";
+import { Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
 import { SupportTicketsComponent } from './dashboard/support-tickets/support-tickets.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
+    declarations: [AppComponent, HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent, DashboardItemComponent],
     imports: [BrowserModule],
     bootstrap: [AppComponent],
   })
