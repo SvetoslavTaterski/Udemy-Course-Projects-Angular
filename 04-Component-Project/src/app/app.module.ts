8a4beef1ca012ac330a8addaf5675235e82d14de
SvetoslavTaterski <svetoslav0247@gmail.com>
2024-07-11 15:14:55 +0300

    Separating the large component into smaller ones

--- 04-Component-Project/src/app/app.module.ts
+++ 04-Component-Project/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ServerStatusComponent } from './server-status/server-status.component';
+import { TrafficComponent } from './traffic/traffic.component';
+import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent],
+    declarations: [AppComponent, HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
     imports: [BrowserModule],
     bootstrap: [AppComponent],
   })
