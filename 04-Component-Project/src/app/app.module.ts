d7dffd800d9f53f8d546b8afef2dd479f064f126
SvetoslavTaterski <svetoslav0247@gmail.com>
2024-07-11 15:18:29 +0300

    Separating the dashboard items components to be in a different folder named dashboard

--- 04-Component-Project/src/app/app.module.ts
+++ 04-Component-Project/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ServerStatusComponent } from './server-status/server-status.component';
-import { TrafficComponent } from './traffic/traffic.component';
-import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { SupportTicketsComponent } from './dashboard/support-tickets/support-tickets.component';
 
 @NgModule({
     declarations: [AppComponent, HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
