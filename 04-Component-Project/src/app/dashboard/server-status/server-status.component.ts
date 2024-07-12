5f7abbd3a7edf1c940d760b5089da488a49013d8
SvetoslavTaterski <svetoslav0247@gmail.com>
2024-07-12 15:25:46 +0300

    Implementing intraface in serverStatusComponent

--- 04-Component-Project/src/app/dashboard/server-status/server-status.component.ts
+++ 04-Component-Project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
 
   constructor() {}
