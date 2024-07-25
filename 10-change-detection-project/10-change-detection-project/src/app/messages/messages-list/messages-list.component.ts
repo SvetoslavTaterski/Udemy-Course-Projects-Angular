f560868b12f84114836b570672483fcf5e12492a
SvetoslavTaterski <svetoslav0247@gmail.com>
2024-07-25 14:29:17 +0300

    feat: Update messages-list component to use getter for messages

--- 10-change-detection-project/10-change-detection-project/src/app/messages/messages-list/messages-list.component.ts
+++ 10-change-detection-project/10-change-detection-project/src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -8,11 +8,22 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent {
-  private messagesService = inject(MessagesService);
-  get messages() {
-    return this.messagesService.allMessages;
+export class MessagesListComponent implements OnInit {
+
+  messages: string[] = [];
+
+  ngOnInit(): void {
+    const subscription = this.messagesService.messages$.subscribe((messages) => {
+      this.messages = messages;
+      this.cdRef.markForCheck();
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
+  private messagesService = inject(MessagesService);
+  private cdRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
