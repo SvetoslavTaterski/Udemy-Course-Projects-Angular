32f6a5d8d0cc05374ca65fc73a3aa4655ecea092
SvetoslavTaterski <svetoslav0247@gmail.com>
2024-07-25 15:23:01 +0300

    Finalizing project 10

--- 10-change-detection-project/10-change-detection-project/src/app/messages/messages-list/messages-list.component.ts
+++ 10-change-detection-project/10-change-detection-project/src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -8,22 +8,9 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent implements OnInit {
-
-  messages: string[] = [];
-
-  ngOnInit(): void {
-    const subscription = this.messagesService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    });
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
+export class MessagesListComponent{
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef);
+  messages = this.messagesService.allMessages;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
