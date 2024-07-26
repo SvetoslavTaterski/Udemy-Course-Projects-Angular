41f3e98db0a57b28c7931621c90576ad04332f22
SvetoslavTaterski <svetoslav0247@gmail.com>
2024-07-26 15:23:33 +0300

    feat: Add loading fallback text for available places component

--- 12-HTTP-Requests-project/12-HTTP-Requests-project/src/app/places/available-places/available-places.component.ts
+++ 12-HTTP-Requests-project/12-HTTP-Requests-project/src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -14,15 +15,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
+    this.isFetching.set(true);
     const subscribtion = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((responseData) => responseData.places)
+      )
       .subscribe({
-        next: (responseData) => {
-          console.log(responseData.places);
+        next: (places) => {
+          this.places.set(places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
         },
       });
 
