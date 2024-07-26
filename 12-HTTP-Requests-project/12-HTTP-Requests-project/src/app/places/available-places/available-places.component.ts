98e22cc9332e94e19c050d181a1f6fb498077994
SvetoslavTaterski <svetoslav0247@gmail.com>
2024-07-26 15:40:48 +0300

    feat: Improve error handling in available-places component

--- 12-HTTP-Requests-project/12-HTTP-Requests-project/src/app/places/available-places/available-places.component.ts
+++ 12-HTTP-Requests-project/12-HTTP-Requests-project/src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -16,6 +16,7 @@ import { map } from 'rxjs';
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
+  error = signal('');
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
@@ -24,7 +25,11 @@ export class AvailablePlacesComponent implements OnInit {
     const subscribtion = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
       .pipe(
-        map((responseData) => responseData.places)
+        map((responseData) => responseData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(() => new Error('An error occurred!'));
+        })
       )
       .subscribe({
         next: (places) => {
@@ -32,6 +37,9 @@ export class AvailablePlacesComponent implements OnInit {
         },
         complete: () => {
           this.isFetching.set(false);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
         },
       });
 
