0382424fdad30fd1b37dbd362342a2d86a676b56
SvetoslavTaterski <svetoslav0247@gmail.com>
2024-08-05 17:46:08 +0300

    Update login form to use Angular forms module

--- 13-forms-project/13-forms-project/src/app/auth/login/login.component.ts
+++ 13-forms-project/13-forms-project/src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
@@ -6,7 +6,15 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { debounceTime, of } from 'rxjs';
 
+let initialEmailValue = '';
+const savedForm = window.localStorage.getItem('saved-login-form');
+
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm);
+  initialEmailValue = loadedForm.email;
+}
 
 @Component({
   selector: 'app-login',
@@ -16,15 +24,22 @@ import {
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private destroyRef = inject(DestroyRef);
+
   form = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.required, Validators.email],
+      asyncValidators: [emailIsUnique],
     }),
     password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark],
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        mustContainQuestionMark,
+      ],
     }),
   });
-  
+
   get emailIsInvalid() {
     return (
       this.form.controls.email.touched &&
@@ -32,7 +47,7 @@ export class LoginComponent {
       this.form.controls.email.invalid
     );
   }
-  
+
   get passwordIsInvalid() {
     return (
       this.form.controls.password.touched &&
@@ -40,13 +55,34 @@ export class LoginComponent {
       this.form.controls.password.invalid
     );
   }
-  
-  
+
   onSubmit() {
     console.log(this.form);
     const enteredEmail = this.form.value.email;
     const enteredPassword = this.form.value.password;
     console.log(enteredEmail, enteredPassword);
+  }
+
+  ngOnInit() {
+    // const savedForm = window.localStorage.getItem('saved-login-form');
+
+    // if (savedForm) {
+    //   const loadedForm = JSON.parse(savedForm);
+    //   this.form.patchValue({
+    //     email: loadedForm.email,
+    //   });
+    // }
+
+    const subscribtion = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: (value) => {
+        window.localStorage.setItem(
+          'saved-login-form',
+          JSON.stringify({ email: value.email })
+        );
+      },
+    });
+
+    this.destroyRef.onDestroy(() => subscribtion.unsubscribe());
   }
 }
 
@@ -56,4 +92,12 @@ function mustContainQuestionMark(control: AbstractControl) {
   }
 
   return { doesNotContainQuestionMark: true };
+}
+
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'test@example.com') {
+    return of(null);
+  }
+
+  return of({ notUnique: true });
 }
